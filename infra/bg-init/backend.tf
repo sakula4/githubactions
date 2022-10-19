f0d48956947f03b7c8c6929734e92e4aecc7b292
Sudheer Akula <sudheer.akula@gainwelltechnologies.com>
2022-10-18 21:47:21 -0500

    test

--- infra/bg-init/backend.tf
+++ infra/bg-init/backend.tf
@@ -6,3 +6,19 @@ terraform {
     dynamodb_table = "github-mygainwell-acuity-tf-state-lockdb"
   }
 }
+
+
+
+data "terraform_remote_state" "state" {
+  backend = "s3"
+  config = {
+    bucket = var.state_bucket_name
+    key    = "gw360/ephermical/infra/ephem/api/us-east-1/terraform.tfstate"
+    region = var.aws_region
+  }
+}
+
+
+output "test" {
+  value = data.terraform_remote_state.state.outputs
+}
