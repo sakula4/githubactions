bfbf269d31f889083f60a5c28f1b2d3e246d2cf3
Sudheer Akula <sudheer.akula@gainwelltechnologies.com>
2022-12-07 17:00:40 -0600

    test

--- scripts/resolveLayers.py
+++ scripts/resolveLayers.py
@@ -2,6 +2,7 @@ import json
 import sys
 
 file = sys.argv[1]
+stackList = sys.argv[2]
 f=open(file)
 enabledKeys = []
 data = json.load(f)
@@ -16,4 +17,7 @@ for x in localData:
                 if (internalKey == 'enabled' and internalData.get('enabled')) :
                     enabledKeys.append(key);
 
-print(enabledKeys)
+# print(enabledKeys)
+new_list = list(set(stackList).intersection(enabledKeys))
+
+print(new_list)
