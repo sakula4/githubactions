52b90ad3008c9e510e999f04cc7ed00a0efa4ff2
Sudheer Akula <sudheer.akula@gainwelltechnologies.com>
2022-12-08 12:33:15 -0600

    update

--- scripts/resolveLayers.py
+++ scripts/resolveLayers.py
@@ -12,14 +12,16 @@ f= open(fileName);
 enabledKeys = []
 data = json.load(f)
 
-localData = data.get('locals');
-for key in localData.keys():
-    internalData = localData.get(key);
-    if type(internalData) is dict:
-        internalKeys = internalData.keys()
-        for internalKey in internalKeys:
-            if (internalKey == 'enabled' and internalData.get('enabled')) :
-                enabledKeys.append(key);
+localData = data['locals']
+for x in localData:
+    keys = x.keys();
+    for key in keys:
+        internalData = localData[0][key];
+        if type(internalData) is dict:
+            internalKeys = internalData.keys()
+            for internalKey in internalKeys:
+                if (internalKey == 'enabled' and internalData.get('enabled')) :
+                    enabledKeys.append(key);
 
 enabledStackLayers = list(set(stackList).intersection(enabledKeys))
 
