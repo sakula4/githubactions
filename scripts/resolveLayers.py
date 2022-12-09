989856d7fb4fe027bc05488fed554214e2a4f092
Sudheer Akula <sudheer.akula@gainwelltechnologies.com>
2022-12-09 12:40:32 -0600

    revert changes

--- scripts/resolveLayers.py
+++ scripts/resolveLayers.py
@@ -5,14 +5,14 @@ import ast;
 
 
 print(os.getenv('metadata_stack'))
-print(os.getenv('update_stack'))
+print(os.getenv('datadog_stack'))
 print(os.getenv('baseline_stack'))
 print(os.getenv('networking_stack'))
 print(os.getenv('tableau_stack'))
 print(os.getenv('datalake_stack'))
 
 metadata_stack = ast.literal_eval(os.getenv('metadata_stack'))
-update_stack = ast.literal_eval(os.getenv('update_stack'))
+datadog_stack = ast.literal_eval(os.getenv('datadog_stack'))
 baseline_stack = ast.literal_eval(os.getenv('baseline_stack'))
 networking_stack = ast.literal_eval(os.getenv('networking_stack'))
 tableau_stack = ast.literal_eval(os.getenv('tableau_stack'))
@@ -20,7 +20,7 @@ abc_stack = ast.literal_eval(os.getenv('abc_stack'))
 datalake_stack = ast.literal_eval(os.getenv('datalake_stack'))
 
 print(metadata_stack)
-print(update_stack)
+print(datadog_stack)
 print(baseline_stack)
 print(networking_stack)
 print(tableau_stack)
@@ -47,7 +47,7 @@ for x in localData:
                     enabledKeys.append(key);
 
 metadata_layers = list(set(metadata_stack).intersection(enabledKeys))
-datadog_layers = list(set(update_stack).intersection(enabledKeys))
+datadog_layers = list(set(datadog_stack).intersection(enabledKeys))
 baseline_layers = list(set(baseline_stack).intersection(enabledKeys))
 networking_layers = list(set(networking_stack).intersection(enabledKeys))
 tableau_layers = list(set(tableau_stack).intersection(enabledKeys))
